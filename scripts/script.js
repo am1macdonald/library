7299e4a7e4629320287d93998b1b5674b03ddc75
Adam <am1macdonald@gmail.com>
2021-04-21 05:50:48 -0400

    built book constructor and default value in myLibrary array

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,23 @@
+let myLibrary = [{
+    author: "King, Stephen", 
+    title: "The Stand",
+    pages: 1000,
+    read: true,
+    rating: 2
+}
+];
 function Book(){
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+    this.rating = rating;
+};
 
+function addBookToLibrary() {
+
+};
+
+function displayBooks(arr) {
+    console.table(myLibrary);
 };
