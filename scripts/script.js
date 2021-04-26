c1ccde828be3b5fe621bfd78c9f6d238fea56dbf
am1macdonald <am1macdonald@gmail.com>
2021-04-26 00:57:13 -0400

    changed logic in new card function to swicth statement

--- scripts/script.js
+++ scripts/script.js
@@ -19,50 +19,44 @@ function Book(author, title, pages, read, rating){
     this.read = read;
     this.rating = rating;
 };
-
 let popup = document.getElementById('popup');
-
 let newBookButton = document.getElementById("adder");
 newBookButton.addEventListener('click', event => {
     popup.style.display = 'flex';
 });
-
 let cancelButton = document.getElementById('cancel');
 cancelButton.addEventListener('click', event => {
     popup.style.display = 'none';
 });
-
 let submitButton = document.getElementById('submit');
 submitButton.addEventListener('click', function() {
-    addBookToLibrary()
+    addBookToLibrary();
+    popup.style.display = 'none';
 });
-
 function addBookToLibrary() {
-
     let author = document.getElementById("author").value;
     let title = document.getElementById('book-name').value;
     let pages = document.getElementById('page-count').value;
-    let read = function() {
-    };
+    let read = document.getElementById('read').checked;    
     let rating;
-
-    let newBook = new Book(author, title, pages, read, rating);
-    
+    let ratingRadios = document.getElementsByName('rating');
+    for (let i = 0; i < 5; i++){
+        if (ratingRadios[i].checked === true) {
+            rating = ratingRadios[i].id;
+            break;
+        };
+    };
+    let newBook = new Book(author, title, pages, read, rating);    
     myLibrary.push(newBook);
-    displayBooks(myLibrary);
-
-    
+    displayBooks(myLibrary);    
 };
-
 let shelf = document.getElementById('library-shelf');
-
 function removeBook(book) {
     let bookIndex = myLibrary.indexOf(book);
     console.log(book, bookIndex);
     myLibrary.splice(bookIndex, 1);
     shelf.childNodes[bookIndex].remove();
 };
-
 function displayBooks(arr) {
     shelf.innerHTML = '';
     let i = 0;
@@ -80,11 +74,18 @@ function displayBooks(arr) {
             removeBook(book)});
         for (let metaData in book){
             let listItem = document.createElement('li');
-            if (metaData === 'read') {
-                book[metaData] === true ? listItem.innerHTML = 'Read' : listItem.innerHTML = 'Not read';
-            } else {                
-                listItem.innerHTML = book[metaData];
-            };
+            switch (metaData) {
+                case 'read':
+                    book[metaData] === true ? listItem.innerHTML = 'Read' : listItem.innerHTML = 'Not read';
+                    break;
+                case 'pages':
+                    listItem.innerHTML = book[metaData] + ' pages';
+                    break;
+                default:
+                    listItem.innerHTML = book[metaData];
+                    break;
+
+            }
             console.log(book[metaData]);
             list.appendChild(listItem); 
         };
